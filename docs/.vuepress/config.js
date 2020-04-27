58e6af64f693a5ed2d44f1a8b65d1ea321b5c66a
code36u4r60 <code36u4r60@hotmail.com>
2020-04-27 15:59:43 +0100

    structure change and some added content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,66 +6,73 @@ module.exports = {
                 '\\Z': '\\mathbb{Z}',
             },
         }],
-        ['vuepress-plugin-container',
-            {
-                type: 'right',
-                defaultTitle: '',
-            },
-        ],
-        ['vuepress-plugin-container',
-            {
-                type: 'theorem',
-                before: info => `<div class="theorem"><p class="title">${info}</p>`,
-                after: '</div>',
-            },
-        ],
+        ['@vuepress/back-to-top', true],
+        ['container', {
+            type: 'vue',
+            before: '<pre class="vue-container"><code>',
+            after: '</code></pre>'
+        }],
+        ['container', {
+            type: 'upgrade',
+            before: info => `<UpgradePath title="${info}">`,
+            after: '</UpgradePath>'
+        }],
     ],
     dest: '/',
     title: 'eSebenta',
     description: 'Solved Exercise Book',
     serviceWorker: true,
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@alias': 'path/to/some/dir'
-            }
-        }
-    },
     theme: 'yuu',
     themeConfig: {
         //logo: '/assets/img/logo.png',
         smoothScroll: true,
+
         lastUpdated: 'Last Updated',
 
-        //repo: 'code36u4r60/my-vuepress',
-        //docsRepo: 'code36u4r60/my-vuepress',
-        //docsDir: 'docs',
-        //editLinks: true,
-        //editLinkText: 'Edit this page on GitHub',
+        repo: 'Sebenta/eSebenta',
+        docsRepo: 'Sebenta/eSebenta',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
 
         nav: [
-            //{ text: 'Programação', link: '/guide/programming/' },
-            { text: 'Electrónica', link: '/guide/electronics/' },
-            { text: 'Redes de Computadores', link: '/guide/networks/' },
+            { text: 'Electrónica', link: '/electronics/' },
+            { text: 'Redes de Computadores', link: '/networks/' },
         ],
+        
         displayAllHeaders: true,
-        sidebarDepth: 3,
+        sidebarDepth: 2,
         sidebar: {
-            '/guide/programming/java/': [
-                '',
-            ],
-            '/guide/programming/c/': [
-                '',
-            ],
-            '/guide/electronics/electronics2/': [
-                ['fet', 'Field Effect Transistor - FET'],
-            ],
-            '/guide/networks/': [
-                //['encaminhamento_de_pacotes _IP','Encaminhamento de Pacotes IP'],
-                //['rip','RIP'],
-                ['trabalhos','Trabalhos Práticos'],
-            ],
-            //'/plugins/': ['',]
+            '/electronics/electronics2/': getGuideSidebar('electronics2', 'Electrónica 2'),
+            '/networks/': getGuideSidebar('networks', 'Redes de Computadores'),
         }
     }
+}
+
+function getGuideSidebar(theme, group) {
+    switch (theme) {
+        case 'electronics2':
+            return [
+                {
+                    title: group,
+                    collapsable: false,
+                    children: [
+                        'fet',
+                    ]
+                }
+            ]
+
+        case 'networks':
+            return [
+                {
+                    title: group,
+                    collapsable: false,
+                    children: [
+                        'trabalhos',
+                    ]
+                }
+            ]
+
+    }
+
 }
